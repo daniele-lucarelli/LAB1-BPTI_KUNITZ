23e010783baa348d6bcb035b17fe7d6b660bcf88
DanieleLucarelli-bit <59140570+DanieleLucarelli-bit@users.noreply.github.com>
2020-05-01 20:35:51 +0200

    Create get_blast_id_retrieve-sequences.py

---
+++ scripts/get_blast_id_retrieve-sequences.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 18:03:26 2020
+
+@author: danielelucarelli
+
+take a blast file and extract the  column (to be specified)
+ 
+than iterate in a fasta file and retrieve the matches
+
+than print the matches to another file
+
+
+It has to be called as:
+python3 get_blast_id_retrieve sequences  <blast file>  <fasta file>  <output file> <integer with the column to extract from blast file>
+
+"""
+
+
+import sys
+
+def get(f1):
+    filein=open(f1,"r")
+    id_li=[]
+    for line in filein:
+        line.rstrip()
+        line=line.split('\t')
+        if line[x] not in id_li:
+            id_li.append(line[x])
+    filein.close()
+    return id_li
+
+def get_seq(f2,id_li):
+    filein=open(f2,"r")
+    content=filein.read()
+    seqs=[]
+    
+    for i in id_li:
+        for j in content.split('>'):
+            if i in j:
+                seqs.append(j)
+    return seqs
+        
+
+
+if __name__=='__main__':
+    x=int (sys.argv[4])-1
+    id_li = get(sys.argv[1])
+    seqs=get_seq(sys.argv[2],id_li)
+    f3= open(sys.argv[3], "w")
+    print (len (id_li))
+    for i in seqs:
+        f3.write('>' + i)
+    f3.close()
